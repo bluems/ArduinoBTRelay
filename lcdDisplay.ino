a2f1369b8f2c8914e367eb63c24ea719af690b8b
나즈나 <nazuna@nazuna.kr>
2016-08-03 04:52:01 +0900

    txt 파일 ino 수정

---
+++ lcdDisplay.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+SoftwareSerial mySerial(2, 4); // RX, TX
+
+
+void setup() {
+  // Open serial communications:
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  
+  Display_Text( 0, "Welcome Delphi.." );
+  
+  // The HC-06 defaults to 9600 according to the datasheet.
+  mySerial.begin(9600);
+}
+
+void loop() {
+
+}
+
+void Display_Text( int LineNo,  String dText ) {
+     //lcd.clear();  
+     switch( LineNo ){
+       case 0 : lcd.setCursor(0, 0); break; 
+       case 1 : lcd.setCursor(0, 1); break;       
+     }
+     lcd.print( dText );  
+          
+     int space = 16 - dText.length();     
+     for( int i = space; i<= 16;  i++ )  lcd.print(" ");  // 여백 채움.
+}
